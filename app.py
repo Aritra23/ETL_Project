4279d007764a50f747ee9f474469e8b5dc85989c
deedee <dinshawdiana@gmail.com>
2019-03-06 23:54:03 -0700

    project ready for submission

--- app.py
+++ app.py
@@ -10,24 +10,13 @@ mongo=PyMongo(app, uri='mongodb://localhost:27017/restaurants_db')
 def index():
     
     return render_template('index.html')
-   
 
-@app.route('/extract')
-def extract():
-    
-    return render_template('index.html')
-
-
-@app.route('/transform')
-def transform():
-    return render_template('index.html')
 
 @app.route('/load')
 def load():
-    mars_data = scrape_mars.scrape()
-    mars= mongo.db.mars
-    mars.update({}, mars_data, upsert=True)
-
+    restaurants = mongo.db.restaurants.find()
+    return render_template("load.html", restaurants=restaurants)
 
 if __name__=='__main__':
     app.run(debug=True)
+
